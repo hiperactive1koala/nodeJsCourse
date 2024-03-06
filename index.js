d895e4a7d6b1a092878309bc1819550e3fd15133
Ismayil Shirinov <sunqurlar@gmail.com>
2024-03-06 23:32:30 +0300

    Frontend added to backend

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ morgan.token('body', getBody = (req) => {
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(morgan(`:method :url :status :res[content-length] - :response-time ms :body`))
 
 app.get('/', (request, response) => {
