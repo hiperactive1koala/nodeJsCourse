164f0861d453fad154f6ded4d8ddf36d583d7f7e
Ismayil Shirinov <sunqurlar@gmail.com>
2024-03-31 00:06:54 +0300

    middleware and error handler added

--- index.js
+++ index.js
@@ -15,11 +15,18 @@ let notes = [
         important: true
     }
 ]
+require('dotenv').config()
+const mongoose = require('mongoose');
 
-const express = require('express');
-const morgan = require('morgan');
+mongoose.set('strictQuery', false)
+mongoose.connect(process.env.MONGODB_URI)
+
 const cors = require('cors');
+const morgan = require('morgan');
+const express = require('express');
 const app = express()
+const Note = require('./models/note')
+
 
 morgan.token('body', getBody = (req) => {
     return JSON.stringify(req.body)
@@ -35,59 +42,87 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(result => {
+        response.json(result)
+    })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id);
-
-    if (note) {
-        response.json(note)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+        .then(note => {
+            if (note) {
+                response.json(note)
+            }
+            else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-
-    response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
-const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
-        : 0
-    return maxId + 1;
-}
+app.put('/api/notes/:id', (request, response, next) => {
+    let { content, important } = request.body
 
-app.post('/api/notes', (request, response) => {
+    Note.findByIdAndUpdate(
+        request.params.id,
+        { content, important },
+        { new: true, runValidators: true, context: 'query' })
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
+})
+
+app.post('/api/notes', (request, response, next) => {
 
     const body = request.body
 
-    if (!body.content) {
-        return response.status(400).json({
-            error: "content missing"
-        })
-    }
+    // if (body.content === undefined) {
+    //     return response.status(400).json({
+    //         error: "content missing"
+    //     })
+    // }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: Boolean(body.important) || false,
-        id: generateId()
-    }
+    })
 
-    notes = notes.concat(note)
-
-    response.json(note);
+    note.save()
+        .then(savedNote => {
+            response.json(savedNote)
+        })
+        .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
+
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+    else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT)
---
+++ mongo.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+
+const mongoose = require('mongoose');
+
+const url =
+    `mongodb+srv://koala:${process.env.PASSWORD}@node-cluster.abxjazt.mongodb.net/noteApp?retryWrites=true&w=majority&appName=node-cluster`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+// const note = new Note({
+//     content: "Football is football what can I do sometimes",
+//     important: false,
+// })
+
+// note.save().then(result => {
+//     console.log('SAVED');
+//     mongoose.connection.close()
+// })
+
+Note.find({}).then(result => {
+    result.forEach(note => {
+        console.log(note);
+    })
+    mongoose.connection.close()
+})
